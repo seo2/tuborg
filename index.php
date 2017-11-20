9c749c4520c4cc06cdbf4adb97041ab0a5082fc3
Seo2 <seo2@seo2.cl>
2017-11-19 21:06:39 -0300

    Primeros pasos

---
+++ index.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <title>Hello, world!</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" >
+    
+	<link href="assets/instashow/jquery.instashow.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/tuborg.css" >
+    
+    
+  </head>
+  <body>
+	<header>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<img src="assets/img/bg-header.png" class="img-fluid">
+				</div>
+			</div>
+		</div>
+	</header>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+				  <ol class="carousel-indicators">
+				    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+				    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+				    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+				    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+				    <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+				  </ol>
+				  <div class="carousel-inner">
+				    <div class="carousel-item active">
+				      <img class="d-block w-100" src="assets/img/slide-5.jpg" alt="First slide">
+				    </div>
+				    <div class="carousel-item">
+				      <img class="d-block w-100" src="assets/img/slide-4.jpg" alt="First slide">
+				    </div>
+				    <div class="carousel-item">
+				      <img class="d-block w-100" src="assets/img/slide-3.jpg" alt="First slide">
+				    </div>
+				    <div class="carousel-item">
+				      <img class="d-block w-100" src="assets/img/slide-2.jpg" alt="First slide">
+				    </div>
+				    <div class="carousel-item">
+				      <img class="d-block w-100" src="assets/img/slide-1.jpg" alt="First slide">
+				    </div>
+				  </div>
+<!--
+				  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				    <span class="sr-only">Previous</span>
+				  </a>
+				  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+				    <span class="sr-only">Next</span>
+				  </a>
+-->
+				</div>				
+			</div>
+		</div>
+		<div class="row">
+			
+		</div>
+        <div class="row">
+			<div data-is
+			    data-is-api="assets/instashow/api/"
+			    data-is-source="@seo2elguerrero"
+			    data-is-width="auto"
+			    data-is-height="500px"
+			    data-is-columns="5"
+			    data-is-rows="3"
+			    data-is-direction="vertical"
+			</div>	
+        </div>
+        <!-- / row -->
+        
+    </div>
+    <footer>
+        
+        
+    </footer>
+	  <!-- Content here -->
+	</div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/3.0.3/handlebars.runtime.min.js"></script>
+	<script src="assets/instashow/jquery.instashow.min.js"></script>
+
+    <script src="assets/js/tuborg.js"></script>
+  </body>
+</html>
