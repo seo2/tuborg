9aa8bf420fc89e2aaa38dc090057d115c7c49d1e
Seo2 <seo2@seo2.cl>
2017-11-21 11:32:06 -0300

    AVANCES

--- assets/js/tuborg.js
+++ assets/js/tuborg.js
@@ -1,1 +1,3 @@
 /* */
+
+  $('.carousel.carousel-slider').carousel({fullWidth: true});
--- index.php
+++ index.php
@@ -1,101 +1,140 @@
 <!doctype html>
 <html lang="es">
   <head>
-    <title>Hello, world!</title>
+    <title>Tuborg</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css" >
+	<!-- Compiled and minified CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+
     
+	<link rel="stylesheet" href="https://use.typekit.net/lkd1qvb.css">
 	<link href="assets/instashow/jquery.instashow.min.css" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/tuborg.css" >
     
+    <link rel="icon" type="image/png" href="assets/img/favicon.png" />
     
   </head>
   <body>
 	<header>
-		<div class="container">
+		<div class="container-fluid">
 			<div class="row">
-				<div class="col-sm-12">
-					<img src="assets/img/bg-header.png" class="img-fluid">
+				<div class="col s12" id="cabecera">
+					<img src="assets/img/logo-barrio_arte.png" id="logo_ba">
+					<img src="assets/img/bg-header.png" class="responsive-img" id="bg-header"> 
 				</div>
 			</div>
 		</div>
 	</header>
-	<div class="container">
+	<div class="container-fluid">
 		<div class="row">
-			<div class="col-sm-12">
-				<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-				  <ol class="carousel-indicators">
-				    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-				    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-				    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-				    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-				    <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
-				  </ol>
-				  <div class="carousel-inner">
-				    <div class="carousel-item active">
-				      <img class="d-block w-100" src="assets/img/slide-5.jpg" alt="First slide">
-				    </div>
-				    <div class="carousel-item">
-				      <img class="d-block w-100" src="assets/img/slide-4.jpg" alt="First slide">
-				    </div>
-				    <div class="carousel-item">
-				      <img class="d-block w-100" src="assets/img/slide-3.jpg" alt="First slide">
-				    </div>
-				    <div class="carousel-item">
-				      <img class="d-block w-100" src="assets/img/slide-2.jpg" alt="First slide">
-				    </div>
-				    <div class="carousel-item">
-				      <img class="d-block w-100" src="assets/img/slide-1.jpg" alt="First slide">
-				    </div>
-				  </div>
-<!--
-				  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-				    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-				    <span class="sr-only">Previous</span>
-				  </a>
-				  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-				    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-				    <span class="sr-only">Next</span>
-				  </a>
--->
+			<div class="col s12">
+				<div class="carousel carousel-slider center" data-indicators="true">
+					<a class="carousel-item" href="http://www.gam.cl" target="_blank"><img src="assets/img/slide-1.jpg"></a>
+					<a class="carousel-item" href="http://www.mnba.cl/" target="_blank"><img src="assets/img/slide-2.jpg"></a>
+					<a class="carousel-item" href="https://www.mavi.cl/" target="_blank"><img src="assets/img/slide-3.jpg"></a>
+					<a class="carousel-item" href="http://www.mac.uchile.cl/" target="_blank"><img src="assets/img/slide-4.jpg"></a>
+					<a class="carousel-item" href="https://www.instagram.com/cervezatuborgchile/" target="_blank"><img src="assets/img/slide-5.jpg"></a>
 				</div>				
 			</div>
 		</div>
-		<div class="row">
-			
+	</div>
+	<div class="container">
+		<div class="row info">
+			<div class="col s3" id="somos">
+				<h2>Somos</h2>
+			</div>
+			<div class="col s9">
+				<p>¡Bienvenidos a BARRIO ARTE!<br>
+					Somos una alianza cultural impulsada por los más importantes Museos de Chile para la promoción de la cultura y las artes en su territorio común. BARRIO ARTE propone un encuentro con la comunidad porque creemos que el desarrollo cultural se logra con el trabajo colaborativo,
+					la generación de pertenencia y la valoración de nuestro patrimonio urbano y arquitectónico.<br>
+					¡No te quedes fuera, súmate y #explorabarrioarte!
+				</p>
+			</div>
 		</div>
-        <div class="row">
-			<div data-is
-			    data-is-api="assets/instashow/api/"
-			    data-is-source="@seo2elguerrero"
-			    data-is-width="auto"
-			    data-is-height="500px"
-			    data-is-columns="5"
-			    data-is-rows="3"
-			    data-is-direction="vertical"
-			</div>	
+		<div class="row info" id="socios">
+			<div class="col s3 valign-wrapper">
+				<h2>Socios</h2>
+			</div>
+			<div class="col s9 valign-wrapper">
+				<p><a href="http://www.gam.cl" target="_blank">gam</a> - <a href="https://www.mavi.cl/" target="_blank">mavi</a> - <a href="http://www.mac.uchile.cl/" target="_blank">mac</a> - <a href="http://www.mnba.cl/" target="_blank">mnba</a></p>
+			</div>
+		</div>
+		<div class="row info" id="participan">
+			<div class="col s3">
+				<h2>Participan</h2>
+			</div>
+			<div class="col s9">
+				<p>El Biógrafo - Galería Plop - Galería Eckho - Metales Pesados - Libros Prólogo - Galería Lira - Tienda Nacional - Librería Ulises - El Cid Campeador - Teatro Ictus - Posada del Corregidor  - Espacio O
+				</p>
+			</div>
+		</div>
+		<div class="row" id="hashtag">
+			<h3><span>#ExploraBarrioArte</span></h3>
+		</div>
+	</div>
+	<div class="container-fluid">
+        <div class="row nomarbot">
+			<div class="col s12">
+				<div data-is
+				    data-is-api="assets/instashow/api/"
+					data-is-source="#cfceurotrip" 
+				    data-is-width="auto"
+				    data-is-height="auto"
+				    data-is-columns="3"
+					data-is-drag-control="true"
+					data-is-effect="slide"
+				    data-is-rows="2"
+				    data-is-gutter="0" 
+				    data-is-direction="horizontal"
+				    data-is-speed="1100" 
+				    data-is-easing="ease-in-out" 
+				    data-is-auto="2000" 
+				    data-is-popup-easing="ease-in-out" 
+				    data-is-lang="es"
+				    data-is-info="likesCounter, description"
+					data-is-color-gallery-overlay="rgba(0,87,167,0.9)"
+					data-is-color-popup-controls ="rgb(0,87,167)"
+					data-is-color-popup-controls-hover ="rgb(0,87,167)"
+					data-is-popup-deep-linking="true"
+					data-is-color-popup-overlay="rgba(255,255,255,.9)"
+					data-is-popup-info="username"
+					data-is-responsive='{ "600": { "columns": 2, "rows": 3, "gutter": 0 }}'
+					data-is-scrollbar="false"
+					data-is-easing="ease-in-out"
+					data-is-arrows-control="false" >	
+<!-- data-is-filter-except="https://www.instagram.com/p/BbrzRL1HKfH/ https://www.instagram.com/p/BbnYdPznPAE/"  -->				
+			</div>
         </div>
         <!-- / row -->
-        
     </div>
-    <footer>
-        
-        
-    </footer>
 	  <!-- Content here -->
 	</div>
+
+    <footer>
+        <div class="container">
+	        <div class="row nomarbot">
+	        	<div class="col s3 valign-wrapper">
+	        		<img src="assets/img/logo-footer-tuborg.png" class="responsive-img" id="logo-tuborg">
+	        	</div>
+	        	<div class="col s9 valign-wrapper">
+	        		<img src="assets/img/logos-footer.png" class="responsive-img">
+	        	</div>
+	        </div>
+        </div>
+    </footer>	
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" ></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+	<!-- Compiled and minified JavaScript -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+	
 
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/3.0.3/handlebars.runtime.min.js"></script>
-	<script src="assets/instashow/jquery.instashow.min.js"></script>
+	<script src="assets/instashow/jquery.instashow.js"></script>
 
     <script src="assets/js/tuborg.js"></script>
   </body>
